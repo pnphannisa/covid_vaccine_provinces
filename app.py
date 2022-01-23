34cb3a95fbe9b306a6b8341648271bb0a0a69243
cstorm125 <cebril@gmail.com>
2022-01-23 12:44:36 +0700

    edit responsiveness for 380px

--- app.py
+++ app.py
@@ -3,24 +3,27 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
 import datetime
+
+
 def hex_to_rgba(h, alpha):
     '''
     converts color value in hex format to rgba format with alpha transparency
     '''
-    return tuple([int(h.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)] + [alpha])
+    return tuple([int(h.lstrip('#')[i:i + 2], 16) for i in (0, 2, 4)] + [alpha])
 
-st.set_page_config (page_title='ผู้ติดเชื้อสะสม vs ผู้ได้รับวัคซีนสะสม')
+
+st.set_page_config(page_title='ผู้ติดเชื้อสะสม vs ผู้ได้รับวัคซีนสะสม')
 st.header('ผู้ติดเชื้อสะสม vs ได้รับวัคซีนสะสม')
 
 st.markdown(
-"""
+    """
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Maitree:wght@500;600&display=swap');
 
 #vs {
     font-family: 'Maitree';
     font-weight: 600;
-    font-size: 48px;
+    font-size: 32px;
 }
 
 .legend {
@@ -35,89 +38,89 @@ html, body {
 
 </style>
 """,
-unsafe_allow_html=True,
+    unsafe_allow_html=True,
 )
 
-#load data
+# load data
 df = pd.read_csv('data/djay_joined_new4.csv')
 df['ymd'] = df.Date.map(lambda x: f'{x.split("/")[2].zfill(2)}-{x.split("/")[1].zfill(2)}-{x.split("/")[0].zfill(2)}')
-	
-#sidebar
+
+# sidebar
 # st.sidebar.write('### เลือกจังหวัดและช่วงเวลา')
 all_provinces = sorted(df.Province_th.unique().tolist())
 province = st.sidebar.selectbox('จังหวัด', all_provinces, index=all_provinces.index('นครนายก'))
-date_begin = st.sidebar.date_input('ตั้งแต่วันที่', value=datetime.date(2021, 1, 1), 
-	min_value=datetime.date(2020, 3, 1), 
-	max_value=datetime.date(2021, 12, 31))
+date_begin = st.sidebar.date_input('ตั้งแต่วันที่', value=datetime.date(2021, 1, 1),
+                                   min_value=datetime.date(2020, 3, 1),
+                                   max_value=datetime.date(2021, 12, 31))
 date_begin = str(date_begin)
-date_end = st.sidebar.date_input('ถึงวันที่', value=datetime.date(2021, 12, 31), 
-	min_value=datetime.date(2020, 3, 1), 
-	max_value=datetime.date(2021, 12, 31))
+date_end = st.sidebar.date_input('ถึงวันที่', value=datetime.date(2021, 12, 31),
+                                 min_value=datetime.date(2020, 3, 1),
+                                 max_value=datetime.date(2021, 12, 31))
 date_end = str(date_end)
 
-#prep data
-df_m = df[df.Province_th==province][['ymd','Province_th',
-    'case_cum_percap',
-    '1vacpercap',
-    '2vacpercap',
-    '3vacpercap']].melt(id_vars=['ymd','Province_th'])
-x = df_m[(df_m.ymd>=date_begin)&(df_m.ymd<=date_end)&(df_m.variable=='case_cum_percap')].ymd
-case_y = df_m[(df_m.ymd>=date_begin)&(df_m.ymd<=date_end)&(df_m.variable=='case_cum_percap')].value
-vaccine1_y = df_m[(df_m.ymd>=date_begin)&(df_m.ymd<=date_end)&(df_m.variable=='1vacpercap')].value
-vaccine2_y = df_m[(df_m.ymd>=date_begin)&(df_m.ymd<=date_end)&(df_m.variable=='2vacpercap')].value
-vaccine3_y = df_m[(df_m.ymd>=date_begin)&(df_m.ymd<=date_end)&(df_m.variable=='3vacpercap')].value
+# prep data
+df_m = df[df.Province_th == province][['ymd', 'Province_th',
+                                       'case_cum_percap',
+                                       '1vacpercap',
+                                       '2vacpercap',
+                                       '3vacpercap']].melt(id_vars=['ymd', 'Province_th'])
+x = df_m[(df_m.ymd >= date_begin) & (df_m.ymd <= date_end) & (df_m.variable == 'case_cum_percap')].ymd
+case_y = df_m[(df_m.ymd >= date_begin) & (df_m.ymd <= date_end) & (df_m.variable == 'case_cum_percap')].value
+vaccine1_y = df_m[(df_m.ymd >= date_begin) & (df_m.ymd <= date_end) & (df_m.variable == '1vacpercap')].value
+vaccine2_y = df_m[(df_m.ymd >= date_begin) & (df_m.ymd <= date_end) & (df_m.variable == '2vacpercap')].value
+vaccine3_y = df_m[(df_m.ymd >= date_begin) & (df_m.ymd <= date_end) & (df_m.variable == '3vacpercap')].value
 
 #global average
-df_global = df[['ymd','Province_th','population','cases_cum',
-    'Vac Given 1 Cum new',
-    'Vac Given 2 Cum new',
-    'Vac Given 3 Cum new']].groupby('ymd').sum().reset_index()
+df_global = df[['ymd', 'Province_th', 'population', 'cases_cum',
+                'Vac Given 1 Cum new',
+                'Vac Given 2 Cum new',
+                'Vac Given 3 Cum new']].groupby('ymd').sum().reset_index()
 df_global['case_cum_percap'] = df_global.cases_cum / df_global.population.max()
-case_y_global = df_global[(df_global.ymd>=date_begin)&(df_global.ymd<=date_end)].case_cum_percap
+case_y_global = df_global[(df_global.ymd >= date_begin) & (df_global.ymd <= date_end)].case_cum_percap
 df_global['1vacpercap'] = df_global['Vac Given 1 Cum new'] / df_global.population.max()
 df_global['2vacpercap'] = df_global['Vac Given 2 Cum new'] / df_global.population.max()
 df_global['3vacpercap'] = df_global['Vac Given 3 Cum new'] / df_global.population.max()
-vaccine1_y_global = df_global[(df_global.ymd>=date_begin)&(df_global.ymd<=date_end)]['1vacpercap'] 
-vaccine2_y_global = df_global[(df_global.ymd>=date_begin)&(df_global.ymd<=date_end)]['2vacpercap']
-vaccine3_y_global = df_global[(df_global.ymd>=date_begin)&(df_global.ymd<=date_end)]['3vacpercap']
+vaccine1_y_global = df_global[(df_global.ymd >= date_begin) & (df_global.ymd <= date_end)]['1vacpercap']
+vaccine2_y_global = df_global[(df_global.ymd >= date_begin) & (df_global.ymd <= date_end)]['2vacpercap']
+vaccine3_y_global = df_global[(df_global.ymd >= date_begin) & (df_global.ymd <= date_end)]['3vacpercap']
 
-#fig 1
+# fig 1
 # Create figure with secondary y-axis
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
 # Add traces
 fig.add_trace(
     go.Scatter(x=x, y=case_y, name="จำนวนผู้ติดเชื้อสะสม/จำนวนประชากร",
-        line=dict(color='#FF5811', width=2,)),
+               line=dict(color='#FF5811', width=2,)),
     secondary_y=False,
 )
 
 fig.add_trace(
     go.Scatter(x=x, y=case_y_global, name="ค่าเฉลี่ยทั่วประเทศ",
-    line=dict(color='rgba' + str(hex_to_rgba(
-                h='#FF5811',
-                alpha=0.5,
-            )),
-            width=2, dash='dot')),
+               line=dict(color='rgba' + str(hex_to_rgba(
+                   h='#FF5811',
+                   alpha=0.5,
+               )),
+                   width=2, dash='dot')),
     secondary_y=False,
 )
 
-# Set x-axis 
+# Set x-axis
 fig.update_xaxes(title_text="")
-fig.update_xaxes(tickfont = {"size": 12, "family": "Maitree"},)
+fig.update_xaxes(tickfont={"size": 12, "family": "Maitree"},)
 fig.update_xaxes(
-    dtick="M1",
+    dtick="M3",
     tickformat="%Y\n%m-%d")
 
-# Set y-axes 
+# Set y-axes
 fig.update_yaxes(showgrid=True, gridwidth=0.25, )
-fig.update_yaxes(tickfont = {"size": 12, "family": "Maitree"},)
+fig.update_yaxes(tickfont={"size": 12, "family": "Maitree"},)
 fig.update_yaxes(zeroline=True, zerolinewidth=0.25, zerolinecolor='#000000')
 # fig.update_yaxes(title_text="จำนวนผู้ได้รับวัคซีนสะสม / จำนวนประชากร", secondary_y=True)
 
 #legend and hover
 fig.update_traces(
-    mode="lines", 
+    mode="lines",
     hoverinfo='y+x',
     # hovermode="x unified",
     # hovertemplate='%{y}',
@@ -125,8 +128,8 @@ fig.update_traces(
 fig.update_layout(
     template='none',
     # plot_bgcolor='#E9E7DD',
-    title = {'text':'จำนวนผู้ติดเชื้อสะสม/จำนวนประชากร','xanchor':'left', 'x':0.05},
-    title_font = {"size": 32, "family": "Maitree"},
+    title={'text': 'จำนวนผู้ติดเชื้อสะสม/จำนวนประชากร', 'xanchor': 'left', 'x': 0.05},
+    title_font={"size": 16, "family": "Maitree"},
     legend=dict(
         yanchor="top",
         y=0.99,
@@ -143,64 +146,64 @@ fig.update_layout(
         font_family="Maitree"
     ),
     margin=dict(l=50, r=50, t=60, b=50),
-    yaxis_tickformat = '.3%'
+    yaxis_tickformat='.3%'
 )
 
 st.plotly_chart(fig, use_container_width=True)
 
 
-#fig 2
+# fig 2
 # Create figure with secondary y-axis
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
 # Add traces
 fig.add_trace(
     go.Scatter(x=x, y=vaccine1_y, name="1 เข็ม",
-        line=dict(color='#4aa5ea', width=2,)),
+               line=dict(color='#4aa5ea', width=2,)),
     secondary_y=False,
 )
 
 fig.add_trace(
     go.Scatter(x=x, y=vaccine2_y, name="2 เข็ม",
-        line=dict(color='#245EF3', width=2,)),
+               line=dict(color='#245EF3', width=2,)),
     secondary_y=False,
 )
 
 fig.add_trace(
     go.Scatter(x=x, y=vaccine1_y_global, name="ค่าเฉลี่ยทั่วประเทศ 1 เข็ม",
-    line=dict(color='rgba' + str(hex_to_rgba(
-                h='#4aa5ea',
-                alpha=0.5,
-            )), 
-    width=2, dash='dot')),
+               line=dict(color='rgba' + str(hex_to_rgba(
+                   h='#4aa5ea',
+                   alpha=0.5,
+               )),
+                   width=2, dash='dot')),
     secondary_y=False,
 )
 
 fig.add_trace(
     go.Scatter(x=x, y=vaccine2_y_global, name="ค่าเฉลี่ยทั่วประเทศ 2 เข็ม",
-    line=dict(color='rgba' + str(hex_to_rgba(
-                h='#245EF3',
-                alpha=0.5,
-            )),
-    width=2, dash='dot')),
+               line=dict(color='rgba' + str(hex_to_rgba(
+                   h='#245EF3',
+                   alpha=0.5,
+               )),
+                   width=2, dash='dot')),
     secondary_y=False,
 )
 
-# Set x-axis 
+# Set x-axis
 fig.update_xaxes(title_text="")
-fig.update_xaxes(tickfont = {"size": 12, "family": "Maitree"},)
+fig.update_xaxes(tickfont={"size": 12, "family": "Maitree"},)
 fig.update_xaxes(
-    dtick="M1",
+    dtick="M3",
     tickformat="%Y\n%m-%d")
 
-# Set y-axes 
+# Set y-axes
 fig.update_yaxes(showgrid=True, gridwidth=0.25, )
-fig.update_yaxes(tickfont = {"size": 12, "family": "Maitree"},)
+fig.update_yaxes(tickfont={"size": 12, "family": "Maitree"},)
 fig.update_yaxes(zeroline=True, zerolinewidth=0.25, zerolinecolor='#000000')
 
 #legend and hover
 fig.update_traces(
-    mode="lines", 
+    mode="lines",
     hoverinfo='y+x',
     # hovermode="x unified",
     # hovertemplate='%{y}',
@@ -208,8 +211,8 @@ fig.update_traces(
 fig.update_layout(
     template='none',
     # plot_bgcolor='#E9E7DD',
-    title = {'text':'จำนวนผู้ได้รับวัคซีน/จำนวนประชากร','xanchor':'left', 'x':0.05},
-    title_font = {"size": 32, "family": "Maitree"},
+    title={'text': 'จำนวนผู้ได้รับวัคซีน/จำนวนประชากร', 'xanchor': 'left', 'x': 0.05},
+    title_font={"size": 16, "family": "Maitree"},
     legend=dict(
         yanchor="top",
         y=0.99,
@@ -226,48 +229,48 @@ fig.update_layout(
         font_family="Maitree"
     ),
     margin=dict(l=50, r=50, t=60, b=50),
-    yaxis_tickformat = '.2%',
+    yaxis_tickformat='.2%',
 )
 
 st.plotly_chart(fig, use_container_width=True)
 
-#fig 3
+# fig 3
 # Create figure with secondary y-axis
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
 # Add traces
 fig.add_trace(
     go.Scatter(x=x, y=vaccine3_y, name="จำนวนผู้ได้รับวัคซีนเข็ม 3/จำนวนประชากร",
-        line=dict(color='#715AFF', width=2,)),
+               line=dict(color='#715AFF', width=2,)),
     secondary_y=False,
 )
 
 fig.add_trace(
     go.Scatter(x=x, y=vaccine3_y_global, name="ค่าเฉลี่ยทั่วประเทศ",
-    line=dict(color='rgba' + str(hex_to_rgba(
-                h='#715AFF',
-                alpha=0.5,
-            )),
-            width=2, dash='dot')),
+               line=dict(color='rgba' + str(hex_to_rgba(
+                   h='#715AFF',
+                   alpha=0.5,
+               )),
+                   width=2, dash='dot')),
     secondary_y=False,
 )
 
-# Set x-axis 
+# Set x-axis
 fig.update_xaxes(title_text="")
-fig.update_xaxes(tickfont = {"size": 12, "family": "Maitree"},)
+fig.update_xaxes(tickfont={"size": 12, "family": "Maitree"},)
 fig.update_xaxes(
-    dtick="M1",
+    dtick="M3",
     tickformat="%Y\n%m-%d")
 
-# Set y-axes 
+# Set y-axes
 fig.update_yaxes(showgrid=True, gridwidth=0.25, )
-fig.update_yaxes(tickfont = {"size": 12, "family": "Maitree"},)
+fig.update_yaxes(tickfont={"size": 12, "family": "Maitree"},)
 fig.update_yaxes(zeroline=True, zerolinewidth=0.25, zerolinecolor='#000000')
 # fig.update_yaxes(title_text="จำนวนผู้ได้รับวัคซีนสะสม / จำนวนประชากร", secondary_y=True)
 
 #legend and hover
 fig.update_traces(
-    mode="lines", 
+    mode="lines",
     hoverinfo='y+x',
     # hovermode="x unified",
     # hovertemplate='%{y}',
@@ -275,8 +278,8 @@ fig.update_traces(
 fig.update_layout(
     template='none',
     # plot_bgcolor='#E9E7DD',
-    title = {'text':'จำนวนผู้ได้รับวัคซีนเข็ม 3/จำนวนประชากร','xanchor':'left', 'x': 0.05},
-    title_font = {"size": 32, "family": "Maitree"},
+    title={'text': 'จำนวนผู้ได้รับวัคซีนเข็ม 3/จำนวนประชากร', 'xanchor': 'left', 'x': 0.05},
+    title_font={"size": 16, "family": "Maitree"},
     legend=dict(
         yanchor="top",
         y=0.99,
@@ -293,12 +296,12 @@ fig.update_layout(
         font_family="Maitree"
     ),
     margin=dict(l=50, r=50, t=60, b=50),
-    yaxis_tickformat = '.2%'
+    yaxis_tickformat='.2%'
 )
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.markdown('ข้อมูลจาก [@djay](https://github.com/djay/covidthailand) รวบรวมจากกรมควบคุมโรค มีการประมาณค่าในวันที่ไม่มีข้อมูล (interpolation)')
+st.markdown('ข้อมูลจาก [@djay](https://github.com/djay/covidthailand) (มีนาคม 2020 - ธันวาคม 2021) รวบรวมจากกรมควบคุมโรค มีการประมาณค่าในวันที่ไม่มีข้อมูล (interpolation)')
 
 # fig.add_trace(
 #     go.Scatter(x=x, y=vaccine2_y, name="จำนวนผู้ได้รับวัคซีน 2 เข็มสะสม / จำนวนประชากร",line_color='green',),
